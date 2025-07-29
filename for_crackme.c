549105018400de18f38a6f962844b0806081b181
Elisey <trex666758@gmail.com>
2025-07-30 02:19:52 +0300

    Create for_crackme.c
    for_crackme code

---
+++ for_crackme.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int x = 20;
+    printf("the value of x is %d\n", x);
+
+    if(x > 5){
+        printf("x is greater then 5.\n");
+    }else{
+        printf("x is not greater than 5.\n");
+    }
+    return 0;
+}
